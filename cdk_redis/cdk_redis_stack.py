842953c08c1d56b73821f9a7fdcda5ce58dcbe20
ytheodosopoulos <yannis.theodosopoulos@gmail.com>
2023-07-07 15:14:45 +0100

    Add code for creating redis resources

--- cdk_redis/cdk_redis_stack.py
+++ cdk_redis/cdk_redis_stack.py
@@ -1,7 +1,10 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_elasticache as elasticache,
+    aws_ec2 as ec2,
+    RemovalPolicy,
+    CfnOutput
 )
 from constructs import Construct
 
@@ -10,10 +13,43 @@ class CdkRedisStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        const developmentVpc = ec2.Vpc.fromLookup(this, 'DevelopmentVPC', { vpcName: 'development' });
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkRedisQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        redis_sg = ec2.SecurxityGroup(
+            self,
+            "redis-sg",
+            security_group_name="redis-sg",
+            vpc=ec2.Vpc.from_lookup(
+                this,
+                'PersonalVPC',
+                tags={
+                    'Environment': 'Development'
+                }
+            ),
+            allow_all_outbound=True
+        )
+
+        private_subnets_ids = [ps.subnet_id for ps in developmentVpc.private_subnets]
+
+        redis_subnet_group = elastiredis_clustercache.CfnSubnetGroup(
+            scope=self,
+            id="redis_subnet_group",
+            subnet_ids=private_subnets_ids,
+            description="subnet group for redis"
+        )
+
+        redis_sec_group.add_ingress_rule(
+            peer='',
+            description="Allow Redis connection",
+            connection=ec2.Port.tcp(6379),            
+        )
+
+        redis_cluster = elasticache.CfnCacheCluster(
+            scope=self,
+            id="redis_cluster",
+            engine="redis",
+            cache_node_type="cache.t3.small",
+            num_cache_nodes=1,
+            cache_subnet_group_name=redis_subnet_group.ref,
+            vpc_security_group_ids=[redis_sec_group.security_group_id],
+        ) 
